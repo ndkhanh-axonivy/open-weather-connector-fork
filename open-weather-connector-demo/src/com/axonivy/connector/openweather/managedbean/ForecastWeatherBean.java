4a8f782bb8f28b43547e5beb4d8a1eaf3aadd58f
khanhduynguyen170900 <khanhduynguyen170900@gmail.com>
2024-01-29 05:02:03 +0700

    Add action for UI

--- open-weather-connector-demo/src/com/axonivy/connector/openweather/managedbean/ForecastWeatherBean.java
+++ open-weather-connector-demo/src/com/axonivy/connector/openweather/managedbean/ForecastWeatherBean.java
@@ -1,15 +1,24 @@
 package com.axonivy.connector.openweather.managedbean;
 
 import java.io.Serializable;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.commons.lang3.ObjectUtils;
 
 import com.axonivy.connector.openweather.data.forecast.ForecastWeatherRecord;
@@ -17,7 +26,10 @@ import com.axonivy.connector.openweather.data.forecast.ForecastWeatherResponse;
 import com.axonivy.connector.openweather.enums.TypeOfDegree;
 import com.axonivy.connector.openweather.exception.OpenWeatherMapException;
 import com.axonivy.connector.openweather.util.DateTimeFormatterUtilities;
-import com.axonivy.connector.openweather.util.FacesContexts;
+
+import ch.ivyteam.ivy.environment.Ivy;
+import ch.ivyteam.ivy.process.call.SubProcessCall;
+import ch.ivyteam.ivy.process.call.SubProcessCallResult;
 
 @ManagedBean
 @ViewScoped
@@ -28,34 +40,67 @@ public class ForecastWeatherBean implements Serializable {
 	private TypeOfDegree typeOfDegree;
 	private LocalDate selectedDate;
 	private LocalTime selectedTime;
+	private int dateIndex;
+	private String cityName;
 	private String formattedTime12Hour;
 	private String formattedDate;
-	private Float currentWeatherDegree;
-	private Float currentHumidity;
-	private Float currentWindSpeed;
+
+	private int currentTemperature;
+	private int currentHumidity;
+	private float currentWindSpeed;
 	private String currentWeatherIconCode;
 	private String currentWeatherDetail;
-	private List<ForecastWeatherRecord> threeHourlyFiveDayForecasts;
+
 	private List<DailyForecast> dailyForecasts;
+
+	private String searchCityName;
+	private String searchCountryCode;
+	private String searchStateCode;
 
 	@PostConstruct
 	public void init() {
-		try {
-			threeHourlyFiveDayForecasts = FacesContexts.evaluateValueExpression(
-					"#{data.forecastWeather.forecastWeatherResponse}", ForecastWeatherResponse.class).getList();
-		} catch (OpenWeatherMapException e) {
-			// Just continue to initialize
-		}
+		searchCityName = "New York";
+		searchCountryCode = "US";
+		searchStateCode = "NY";
+		search();
 	}
 
 	public LocalDate getSelectedDate() {
 		return selectedDate;
 	}
 
+	public void setSelectedDateByJsf() {
+
+		String selectedDateStr = FacesContext.getCurrentInstance().getExternalContext().getRequestParameterMap()
+				.get("selectedDate");
+		Ivy.log().info(selectedDateStr);
+		if (selectedDateStr != null && !selectedDateStr.isEmpty()) {
+			LocalDate selectedDate = LocalDate.parse(selectedDateStr);
+			setSelectedDate(selectedDate);
+			// Now 'selectedDate' contains the parsed LocalDate value
+		} else {
+			// Handle the case where 'selectedDateStr' is null or empty
+		}
+	}
+
 	public void setSelectedDate(LocalDate selectedDate) {
+		Ivy.log().info(selectedDate.toString());
 		this.selectedDate = selectedDate;
 		updateFormattedDate();
 		setSelectedTime(null);
+
+		dateIndex = IntStream.range(0, dailyForecasts.size())
+				.filter(i -> dailyForecasts.get(i).getDate().equals(selectedDate)).findFirst().orElse(-1);
+
+		if (dateIndex != -1) {
+			// Selected date found, use the index to get the selectedDailyForecast
+			DailyForecast selectedForecast = dailyForecasts.get(dateIndex);
+			currentTemperature = selectedForecast.getTemperature();
+			currentHumidity = selectedForecast.getHumidity();
+			currentWindSpeed = selectedForecast.getWindSpeed();
+			currentWeatherIconCode = selectedForecast.getWeatherIcon();
+			currentWeatherDetail = StringUtils.capitalize(selectedForecast.getWeatherDescription());
+		}
 	}
 
 	public LocalTime getSelectedTime() {
@@ -71,15 +116,15 @@ public class ForecastWeatherBean implements Serializable {
 		return typeOfDegree;
 	}
 
-	public Float getCurrentWeatherDegree() {
-		return currentWeatherDegree;
+	public int getCurrentWeatherDegree() {
+		return currentTemperature;
 	}
 
-	public Float getCurrentHumidity() {
+	public int getCurrentHumidity() {
 		return currentHumidity;
 	}
 
-	public Float getCurrentWindSpeed() {
+	public float getCurrentWindSpeed() {
 		return currentWindSpeed;
 	}
 
@@ -99,6 +144,30 @@ public class ForecastWeatherBean implements Serializable {
 		return formattedDate;
 	}
 
+	public String getSearchCityName() {
+		return searchCityName;
+	}
+
+	public void setSearchCityName(String searchCityName) {
+		this.searchCityName = searchCityName;
+	}
+
+	public String getSearchCountryCode() {
+		return searchCountryCode;
+	}
+
+	public void setSearchCountryCode(String searchCountryCode) {
+		this.searchCountryCode = searchCountryCode;
+	}
+
+	public String getSearchStateCode() {
+		return searchStateCode;
+	}
+
+	public void setSearchStateCode(String searchStateCode) {
+		this.searchStateCode = searchStateCode;
+	}
+
 	private void updateFormattedDate() {
 		formattedDate = DateTimeFormatterUtilities.formatDate(selectedDate);
 	}
@@ -107,45 +176,128 @@ public class ForecastWeatherBean implements Serializable {
 		formattedTime12Hour = DateTimeFormatterUtilities.formatTime12Hour(selectedTime);
 	}
 
-	private void calculateDailyForecasts() {
-		if (ObjectUtils.isEmpty(threeHourlyFiveDayForecasts)) {
-			return;
-		}
-
-		// Group forecasts by date
-		Map<LocalDate, List<ForecastWeatherRecord>> forecastsByDate = threeHourlyFiveDayForecast.stream()
-				.collect(Collectors.groupingBy(record -> record.getDt().toLocalDate()));
-
-		// Calculate daily forecasts
-		dailyForecasts = new ArrayList<>();
-		for (Map.Entry<LocalDate, List<ForecastWeatherRecord>> entry : forecastsByDate.entrySet()) {
-			LocalDate date = entry.getKey();
-			List<ForecastWeatherRecord> dailyRecords = entry.getValue();
-
-			float maxTemperature = dailyRecords.stream().map(ForecastWeatherRecord::getMain).map(Main::getTempMax)
-					.max(Float::compareTo).orElse(0.0f);
-
-			float maxHumidity = dailyRecords.stream().map(ForecastWeatherRecord::getMain).map(Main::getHumidity)
-					.max(Float::compareTo).orElse(0.0f);
-
-			dailyForecasts.add(new DailyForecast(date, dailyRecords, maxTemperature, maxHumidity));
-		}
+	public String getCityName() {
+		return cityName;
 	}
 
-	private static class DailyForecast {
-		private final LocalDate date;
-		private final List<ForecastWeatherRecord> dailyRecords;
-		private final float maxTemperature;
-		private final float maxHumidity;
+	public List<DailyForecast> getDailyForecasts() {
+		return dailyForecasts;
+	}
+	
+	public int getDateIndex() {
+		return dateIndex;
+	}
 
-		private DailyForecast(LocalDate date, List<ForecastWeatherRecord> dailyRecords, float maxTemperature,
-				float maxHumidity) {
+	public void search() {
+		ForecastWeatherResponse forecastWeather = null;
+		Ivy.log().info("searchCityName: " + searchCityName);
+		SubProcessCallResult callResult = SubProcessCall.withPath("ForecastWeather").withStartName("call")
+				.withParam("cityName", searchCityName).withParam("countryCode", searchCountryCode)
+				.withParam("stateCode", searchStateCode).call();
+		if (callResult != null) {
+			Optional<Object> o = Optional.ofNullable(callResult.get("response"));
+			if (o.isPresent() && o.get() instanceof ForecastWeatherResponse object) {
+				forecastWeather = (ForecastWeatherResponse) object;
+			}
+		}
+		cityName = forecastWeather.getCity().getName();
+		List<ForecastWeatherRecord> threeHourlyFiveDayForecasts = forecastWeather.getList();
+		Ivy.log().info(threeHourlyFiveDayForecasts.size());
+		// Group forecasts by date
+		Map<LocalDate, List<ForecastWeatherRecord>> forecastsByDate = threeHourlyFiveDayForecasts.stream()
+				.collect(Collectors.groupingBy(
+						record -> Instant.ofEpochSecond(record.getDt()).atZone(ZoneId.systemDefault()).toLocalDate()));
+
+		dailyForecasts = forecastsByDate.entrySet().stream()
+				.map(entry -> new DailyForecast(entry.getKey(), entry.getValue()))
+				.sorted(Comparator.comparing(DailyForecast::getDate)).limit(5).collect(Collectors.toList());
+
+		dateIndex = 0;
+		setSelectedDate(dailyForecasts.get(dateIndex).date);
+	}
+
+	public static class DailyForecast {
+		private final LocalDate date;
+		private String formattedEEE;
+		private final List<ForecastWeatherRecord> dailyRecords;
+		private final int temperature;
+		private final int minTemperature;
+		private final int maxTemperature;
+		private final int humidity;
+		private final float windSpeed;
+		private final String weatherIcon;
+		private final String weatherDescription;
+		private static final String DEFAULT_WEATHER_CONDITION_ICON = "01d";
+		private static final String DEFAULT_WEATHER_CONDITION_DESCRIPTION = "clear sky";
+
+		private DailyForecast(LocalDate date, List<ForecastWeatherRecord> dailyRecords) {
 			this.date = date;
 			this.dailyRecords = dailyRecords;
-			this.maxTemperature = maxTemperature;
-			this.maxHumidity = maxHumidity;
+
+			formattedEEE = DateTimeFormatterUtilities.formatEEE(date);
+			temperature = dailyRecords.stream().map(ForecastWeatherRecord::getMainTemp).mapToInt(Float::intValue).max()
+					.orElse(0);
+			maxTemperature = dailyRecords.stream().map(ForecastWeatherRecord::getMainMaxTemp).mapToInt(Float::intValue)
+					.max().orElse(0);
+			minTemperature = dailyRecords.stream().map(ForecastWeatherRecord::getMainMinTemp).mapToInt(Float::intValue)
+					.min().orElse(0);
+			humidity = dailyRecords.stream().map(ForecastWeatherRecord::getMainHumidity).max(Integer::compareTo)
+					.orElse(0);
+			windSpeed = dailyRecords.stream().map(ForecastWeatherRecord::getWindSpeed).max(Float::compareTo)
+					.orElse(0.0f);
+			weatherIcon = dailyRecords.stream()
+					.max(Comparator.comparingInt(record -> getWeatherPriority(record.getWeatherId())))
+					.map(ForecastWeatherRecord::getWeatherIcon).orElse(DEFAULT_WEATHER_CONDITION_ICON);
+			weatherDescription = dailyRecords.stream()
+					.max(Comparator.comparingInt(record -> getWeatherPriority(record.getWeatherId())))
+					.map(ForecastWeatherRecord::getWeatherDescription).orElse(DEFAULT_WEATHER_CONDITION_DESCRIPTION);
 		}
 
-		// Getters for properties
+		public LocalDate getDate() {
+			return date;
+		}
+
+		public List<ForecastWeatherRecord> getDailyRecords() {
+			return dailyRecords;
+		}
+
+		public int getTemperature() {
+			return temperature;
+		}
+
+		public int getMaxTemperature() {
+			return maxTemperature;
+		}
+
+		public int getMinTemperature() {
+			return minTemperature;
+		}
+
+		public int getHumidity() {
+			return humidity;
+		}
+
+		public float getWindSpeed() {
+			return windSpeed;
+		}
+
+		public String getWeatherIcon() {
+			return weatherIcon;
+		}
+
+		public String getWeatherDescription() {
+			return weatherDescription;
+		}
+
+		public String getFormattedEEE() {
+			return formattedEEE;
+		}
+
+		private static int getWeatherPriority(int weatherId) {
+			int firstDigit = weatherId / 100;
+			int lastTwoDigits = weatherId % 100;
+
+			return firstDigit * -100 + lastTwoDigits;
+		}
 	}
 }
--- open-weather-connector-demo/src/com/axonivy/connector/openweather/util/DateTimeFormatterUtilities.java
+++ open-weather-connector-demo/src/com/axonivy/connector/openweather/util/DateTimeFormatterUtilities.java
@@ -9,6 +9,7 @@ import org.apache.commons.lang3.StringUtils;
 public class DateTimeFormatterUtilities {
 	private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("E, d MMMM yyyy");
 	private static final DateTimeFormatter TIME_12_HOUR_FORMATTER = DateTimeFormatter.ofPattern("h:mm a");
+	private static final DateTimeFormatter EEE_FORMATTER = DateTimeFormatter.ofPattern("EEE");
 
 	public static String formatDate(LocalDate date) {
 		return date != null ? date.format(DATE_FORMATTER) : StringUtils.EMPTY;
@@ -16,5 +17,9 @@ public class DateTimeFormatterUtilities {
 
 	public static String formatTime12Hour(LocalTime time) {
 		return time != null ? time.format(TIME_12_HOUR_FORMATTER) : StringUtils.EMPTY;
+	}
+	
+	public static String formatEEE(LocalDate date) {
+		return date != null ? date.format(EEE_FORMATTER) : StringUtils.EMPTY;
 	}
 }
