6794a9d9f08eab811f0f78599fe1181856a2efd7
ndkhanh-axonivy <khanh.nguyenduy@axonactive.com>
2024-08-15 01:39:55 +0700

    Update PriorityConcatenationUtilities.java

--- open-weather-connector/src/com/axonivy/connector/openweather/util/PriorityConcatenationUtilities.java
+++ open-weather-connector/src/com/axonivy/connector/openweather/util/PriorityConcatenationUtilities.java
@@ -23,7 +23,6 @@ public class PriorityConcatenationUtilities {
 	private static int getFirstBlankIndex(List<String> strings) {
 		for (int i = 0; i < strings.size(); i++) {
 			if (StringUtils.isBlank(strings.get(i))) {
-				i += 1;
 				return i;
 			}
 		}
